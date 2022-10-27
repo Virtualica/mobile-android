6ed5178a56481741f825ca57212ce3e18408fd42
GabrielSB19 <gasuarez2002@gmail.com>
2022-10-26 20:48:07 -0500

    Starting upload the photo profile

--- app/src/main/java/com/virtualica/mobile_android/CategoryFragment.kt
+++ app/src/main/java/com/virtualica/mobile_android/CategoryFragment.kt
@@ -5,9 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ListView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.virtualica.mobile_android.databinding.CategoryListBinding
 
--- app/src/main/java/com/virtualica/mobile_android/ProfileActivity.kt
+++ app/src/main/java/com/virtualica/mobile_android/ProfileActivity.kt
@@ -1,12 +1,29 @@
 package com.virtualica.mobile_android
 
+import android.Manifest
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.WindowCompat
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.profile.*
 
-class ProfileActivity : AppCompatActivity() {
+class ProfileActivity : AppCompatActivity(),  ActivityCompat.OnRequestPermissionsResultCallback {
+
+    private val REQUEST_PERMISSION = 100
+    private var imageUri: Uri? = null
+    private val db = Firebase.firestore
+    //private val storage = Firebase.storage
 
     private lateinit var profile: ImageView
     private lateinit var logo: ImageView
@@ -26,6 +43,36 @@ class ProfileActivity : AppCompatActivity() {
         logo.setOnClickListener {
             val intent = Intent(this, FragmentActivity::class.java)
             startActivity(intent)
+        }
+
+        imagePicture.setOnClickListener {
+            checkExternalStoragePermission()
+            openGallery()
+            Log.e("Error", "Sapa")
+        }
+    }
+
+    private fun checkExternalStoragePermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                REQUEST_PERMISSION
+            )
+        }
+    }
+
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startGalleryForResult.launch(intent)
+    }
+
+    private val startGalleryForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if (it.resultCode == Activity.RESULT_OK) {
+            imageUri = it.data?.data
+            Log.e("Error", "URL"+imageUri.toString())
         }
     }
 }
