13444acf6e52a16b24daa82bd5306ce8bb9fe233
David Montaño <cuatro04sr@gmail.com>
2022-11-15 19:20:22 -0500

    feat: add gallery and load firestore cloud, fix app bar

--- app/src/main/java/com/virtualica/mobile_android/ProfileActivity.kt
+++ app/src/main/java/com/virtualica/mobile_android/ProfileActivity.kt
@@ -14,16 +14,20 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.WindowCompat
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.storage.ktx.storage
 import com.google.firebase.ktx.Firebase
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.profile.*
+import kotlin.math.log
 
-class ProfileActivity : AppCompatActivity(),  ActivityCompat.OnRequestPermissionsResultCallback {
+class ProfileActivity : AppCompatActivity(), ActivityCompat.OnRequestPermissionsResultCallback {
 
     private val REQUEST_PERMISSION = 100
     private var imageUri: Uri? = null
     private val db = Firebase.firestore
-    //private val storage = Firebase.storage
+    private val storage = Firebase.storage
 
     private lateinit var profile: ImageView
     private lateinit var logo: ImageView
@@ -33,35 +37,40 @@ class ProfileActivity : AppCompatActivity(),  ActivityCompat.OnRequestPermission
         super.onCreate(savedInstanceState)
         setContentView(R.layout.profile)
 
-        profile = findViewById(R.id.profile)
-        profile.setOnClickListener {
-            val intent = Intent(this, ProfileActivity::class.java)
-            startActivity(intent)
-        }
-
         logo = findViewById(R.id.logo)
         logo.setOnClickListener {
             val intent = Intent(this, FragmentActivity::class.java)
             startActivity(intent)
         }
 
-        imagePicture.setOnClickListener {
+        profile = findViewById(R.id.profile)
+        profile.setOnClickListener {
             checkExternalStoragePermission()
-            openGallery()
-            Log.e("Error", "Sapa")
         }
     }
 
     private fun checkExternalStoragePermission() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
-            != PackageManager.PERMISSION_GRANTED
-        ) {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED
+        )
+            openGallery()
+        else
             ActivityCompat.requestPermissions(
                 this,
                 arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
                 REQUEST_PERMISSION
             )
-        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_PERMISSION && grantResults.isNotEmpty() && grantResults[0] == 0)
+            openGallery()
     }
 
     private fun openGallery() {
@@ -69,10 +78,38 @@ class ProfileActivity : AppCompatActivity(),  ActivityCompat.OnRequestPermission
         startGalleryForResult.launch(intent)
     }
 
-    private val startGalleryForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-        if (it.resultCode == Activity.RESULT_OK) {
-            imageUri = it.data?.data
-            Log.e("Error", "URL"+imageUri.toString())
+    private val startGalleryForResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == Activity.RESULT_OK) {
+                imageUri = it.data?.data
+                uploadImage { imageUrl ->
+                    Log.e("Error", "URL2$imageUrl")
+                    if (imageUrl != null) {
+                        Picasso.get().load(Uri.parse(imageUrl)).into(profile)
+                    }
+                }
+                Log.e("Error", "URL" + imageUri.toString())
+            }
+        }
+
+    private fun uploadImage(completion: (String?) -> Unit) {
+        if (this.imageUri != null) {
+            val storageRef = storage.reference
+            val reportRef = storageRef.child("profile_photo/test.jpg")
+            val uploadTask = reportRef.putFile(imageUri!!)
+            uploadTask.addOnFailureListener {
+                completion(null)
+            }.addOnSuccessListener {
+                reportRef.downloadUrl
+                    .addOnSuccessListener {
+                        completion(it.toString())
+                    }
+                    .addOnCanceledListener {
+                        completion(null)
+                    }
+            }
+        } else {
+            completion(null)
         }
     }
 }
